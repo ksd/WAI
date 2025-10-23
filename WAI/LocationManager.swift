ec0df26766b366e987488e8706e08f6b509003b6
Kaj Schermer Didriksen <kdidriksen@me.com>
2025-10-23 13:02:30 +0200

    Opgave 2 løst næsten

--- WAI/LocationManager.swift
+++ WAI/LocationManager.swift
@@ -1,11 +1,11 @@
 // LocationManager.swift
-// Project: WAI 
+// Project: WAI
 // Compiled with Swift version 6.0
 //
 // Created by ksd/Kaj Schermer Didriksen on 21/10/2025 at 11.18.
 // Copyright © 2025 ksd. All rights reserved.
 //
-// 
+//
 
 
 import Foundation
@@ -15,6 +15,12 @@ import MapKit
 class LocationManager: NSObject {
 
     var userLocation: CLLocation?
+
+    let dataSet = [
+        User(coordinate: CLLocationCoordinate2D(latitude: 56.119821, longitude: 10.158652), tag: 0),
+        User(coordinate: CLLocationCoordinate2D(latitude: 56.442701, longitude: 10.148295), tag: 1),
+    ]
+
     private var locationManager = CLLocationManager()
 
     override init(){
@@ -30,6 +36,8 @@ extension LocationManager: CLLocationManagerDelegate {
         didUpdateLocations locations: [CLLocation]
     ) {
         userLocation = locations.last
+        ///find and update the user in the dataSet
+
     }
 
     func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
--- WAI/User.swift
+++ WAI/User.swift
@@ -1,3 +1,6 @@
+import Foundation
+import CoreLocation
+
 struct User: Identifiable {
     let coordinate: CLLocationCoordinate2D
     let tag: Int
