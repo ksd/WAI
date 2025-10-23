3867dcb61df181852b0244543fcc1794774c7fa2
Kaj Schermer Didriksen <kdidriksen@me.com>
2025-10-23 13:03:19 +0200

    Rettet et par småfejl

--- WAI/ContentView.swift
+++ WAI/ContentView.swift
@@ -1,60 +1,70 @@
 // ContentView.swift
-// Project: WAI 
+// Project: WAI
 // Compiled with Swift version 6.0
 //
 // Created by ksd/Kaj Schermer Didriksen on 21/10/2025 at 11.17.
 // Copyright © 2025 ksd. All rights reserved.
 //
-// 
+//
 
 import SwiftUI
 import MapKit
 
-struct User: Identifiable {
-    let coordinate: CLLocationCoordinate2D
-    let tag: Int
-    var id: Int {tag}
-}
+
 
 struct ContentView: View {
     @State private var selectedMarker: Int?
     @State private var route: MKRoute?
     @Environment(LocationManager.self) var locationManager
 
-    let dataSet = [
-        User(coordinate: CLLocationCoordinate2D(latitude: 56.119821, longitude: 10.158652), tag: 0),
-        User(coordinate: CLLocationCoordinate2D(latitude: 56.442701, longitude: 10.148295), tag: 1)
-    ]
-
     var body: some View {
 
         Map(selection: $selectedMarker){
 
-            ForEach(dataSet) { user in
+            ForEach(locationManager.dataSet) { user in
                 Marker(coordinate: user.coordinate) {
                     Image(systemName: "person.fill")
                         .font(.system(size: 32))
-                        .foregroundColor(.blue)
                 }.tag(user.tag)
+                    .tint(.green)
             }
+            if let userLocation = locationManager.userLocation {
+                Marker(
+                    coordinate: CLLocationCoordinate2D(
+                        latitude: userLocation.coordinate.latitude,
+                        longitude: userLocation.coordinate.longitude)
+                ) {
+                    Image(systemName: "person.fill")
+                        .font(.system(size: 32))
 
+                }.tag(2)
+                    .tint(.blue)
+            }
             if let route {
                 MapPolyline(route)
                     .stroke(.blue, lineWidth: 5)
             }
 
         }
-            .edgesIgnoringSafeArea(.all)
-            .onChange(of: selectedMarker) { oldValue, newValue in
-                guard let newValue, let oldValue else { return }
-                let source = getLocationFrom(tag: oldValue)
-                let destination = getLocationFrom(tag: newValue)
-                guard let source, let destination else { return }
-                getDirections(source: source, destination: destination)
-            }
+        .edgesIgnoringSafeArea(.all)
+
+        /// viser på kortet, hvor du er. Hvis du har givet lov:)
+        .mapControls({
+            MapCompass()
+            MapUserLocationButton()
+        })
+        .controlSize(.large)
+
+        .onChange(of: selectedMarker) { oldValue, newValue in
+            guard let newValue, let oldValue else { return }
+            let source = getLocationFrom(tag: oldValue)
+            let destination = getLocationFrom(tag: newValue)
+            guard let source, let destination else { return }
+            getDirections(source: source, destination: destination)
+        }
     }
     private func getLocationFrom(tag: Int) -> CLLocation?{
-        let user = dataSet.first{$0.tag == tag}
+        let user = locationManager.dataSet.first{$0.tag == tag}
         guard let user else { return nil }
         return CLLocation(
             latitude: user.coordinate.latitude,
@@ -62,9 +72,9 @@ struct ContentView: View {
         )
     }
 
-   private func getDirections(source: CLLocation, destination: CLLocation) {
+    private func getDirections(source: CLLocation, destination: CLLocation) {
         route = nil
-       selectedMarker = nil
+        selectedMarker = nil
         let request = MKDirections.Request()
         request.transportType = .walking
         request.source = MKMapItem(
